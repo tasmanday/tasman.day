d355d1384ac3f93edf820206a847b06f2a15e483
Tasman <tasmanday@users.noreply.github.com>
2025-07-24 21:56:57 +0930

    added loading message

--- js/main.js
+++ js/main.js
@@ -4,4 +4,6 @@ import { initCanvas } from './canvas.js';
 document.addEventListener('DOMContentLoaded', () => {
 	createJumble();
 	initCanvas();
+	document.querySelector('.home-content-container').style.display = 'block';
+	document.getElementById('loading-message').style.display = 'none';
 });
